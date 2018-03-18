eca1526f66e0a68e229fadf425966790551ce3e0
toblich <toblich@gmail.com>
2018-03-18 20:39:15 -0300

    Add terraform for instance with node app

---
+++ security_groups.tf
@@ -0,0 +1,29 @@
+resource "aws_security_group" "apps" {
+  name        = "Main SG for apps"
+  description = "Allow SSH and traffic on port 3000 from anywhere"
+  vpc_id      = "${var.vpc_id}"
+
+  ingress {
+    description = "SSH"
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  ingress {
+    description = "Traffic"
+    from_port   = 3000
+    to_port     = 3000
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  egress {
+    description = "Allow all outgoing traffic"
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+}
