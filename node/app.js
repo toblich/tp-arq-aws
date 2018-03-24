7e3376a57c334e82970ecc0154169d92c5335e8c
toblich <toblich@gmail.com>
2018-03-24 17:19:02 -0300

    Add connect-datadog to node app

--- node/app.js
+++ node/app.js
@@ -1,6 +1,17 @@
 const express = require('express')
 const app     = express()
 
+const datadogOptions = {
+  'response_code': true,
+  'tags':          ['app:node']
+}
+
+const connectDatadog = require('connect-datadog')(datadogOptions);
+
+app.use(connectDatadog);
+
+// ---
+
 app.get('/', (req, res) => res.send('Hello World!'))
 
 app.listen(3000, () => console.log('Example app listening on port 3000!'))
