41e9fc00e215edc2346a4487e8139c829b0184f7
toblich <toblich@gmail.com>
2018-03-25 16:47:21 -0300

    Add python machine with python gunicorn app

---
+++ python.tf
@@ -0,0 +1,94 @@
+resource "aws_instance" "python" {
+  ami                    = "${var.ami_id}"
+  instance_type          = "t2.micro"
+  key_name               = "${var.key_pair_name}"
+  vpc_security_group_ids = ["${aws_security_group.apps.id}"]
+
+  tags {
+    Name = "tp_arqui_python"
+  }
+
+  # Store the resulting IP
+  provisioner "local-exec" {
+    command = "echo ${aws_instance.python.public_ip} > python/ip"
+  }
+
+  # Environment setup
+  provisioner "remote-exec" {
+    inline = [
+      "echo ------- Create application directory: ${var.root} -------",
+      "mkdir ${var.root}",
+    ]
+
+    connection {
+      type        = "ssh"
+      user        = "ec2-user"
+      private_key = "${file(var.private_key_location)}"
+    }
+  }
+
+  # Upload installer script
+  provisioner "file" {
+    source      = "install_datadog.sh"
+    destination = "~/install_datadog.sh"
+
+    connection {
+      type        = "ssh"
+      user        = "ec2-user"
+      private_key = "${file(var.private_key_location)}"
+    }
+  }
+
+  # Execute installer script
+  provisioner "remote-exec" {
+    inline = [
+      "chmod +x ~/install_datadog.sh",
+      "~/install_datadog.sh ${var.datadog_key}",
+    ]
+
+    connection {
+      type        = "ssh"
+      user        = "ec2-user"
+      private_key = "${file(var.private_key_location)}"
+    }
+  }
+
+  # Upload app
+  provisioner "file" {
+    source      = "python/app.py"
+    destination = "${var.root}/app.py"
+
+    connection {
+      type        = "ssh"
+      user        = "ec2-user"
+      private_key = "${file(var.private_key_location)}"
+    }
+  }
+
+  provisioner "file" {
+    source      = "python/requirements.txt"
+    destination = "${var.root}/requirements.txt"
+
+    connection {
+      type        = "ssh"
+      user        = "ec2-user"
+      private_key = "${file(var.private_key_location)}"
+    }
+  }
+
+  # Install app deps and run
+  provisioner "remote-exec" {
+    inline = [
+      "echo ------- Move into application directory: ${var.root} -------",
+      "cd ${var.root}",
+      "echo ------- Install dependencies -------",
+      "sudo pip install -r requirements.txt",
+    ]
+
+    connection {
+      type        = "ssh"
+      user        = "ec2-user"
+      private_key = "${file(var.private_key_location)}"
+    }
+  }
+}
